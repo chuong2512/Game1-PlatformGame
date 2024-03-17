9d88685b110c67d97643ac9f8b9be3fc56deba72
chuong_gamer <hieutg244@gmail.com>
2024-03-17 11:22:40 +0700

    update

---
+++ Assets/Kong Hero/Script/NotEnoughCoins.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotEnoughCoins : MonoBehaviour {
+	public static NotEnoughCoins Instance;
+	public GameObject Panel;
+
+	void Awake(){
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Panel.SetActive (false);
+	}
+
+    public void ShowUp()
+    {
+        #if UNITY_ANDROID || UNITY_IOS
+        if (AdsManager.Instance && AdsManager.Instance.isRewardedAdReady())
+            Panel.SetActive(true);
+#endif
+    }
+
+    public void Close(){
+		Panel.SetActive (false);
+	}
+}
